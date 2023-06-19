104b0a9e30df46e4192d5948d2901b13731f0b6e
Saken Myrzabekov <myrzabekovsaken89@gmail.com>
2023-06-19 14:47:11 +0600

    feat: update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram import Bot, Dispatcher, types, executor
+from aiogram import Bot, Dispatcher, executor
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup 
 import json
 import random
 import logging
@@ -21,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 def get_database():
     MONGO_URL = os.getenv("MONGO_URL")
     client = MongoClient(MONGO_URL)
-    return client["t-bot-result"]
+    return client["test"]
 
 
 bot = Bot(token=API_TOKEN)
@@ -31,7 +32,7 @@ dp = Dispatcher(bot, storage=storage)
 
 def load_scores():
     database = get_database()
-    t_bot_result_collection = database["t-bot-result"]
+    t_bot_result_collection = database["guess_number_result"]
     t_bot_document = t_bot_result_collection.find_one({})
     if t_bot_document:
         scores = t_bot_document.get("data", {})
@@ -39,45 +40,35 @@ def load_scores():
         scores = {}
     return scores
 
-    # try:
-    #     with open('scores.json', 'r') as file:
-    #         scores = json.load(file)
-    # except (FileNotFoundError, json.decoder.JSONDecodeError):
-    #     scores = {}
-    # return scores
-
 
 def save_scores(scores):
     database = get_database()
-    t_bot_result_collection = database["t-bot-result"]
+    t_bot_result_collection = database["guess_number_result"]
     t_bot_result_collection.update_one({}, {"$set": {"data": scores}}, upsert=True)
-
-    # with open('scores.json', 'w') as file:
-    #     json.dump(scores, file)
 
 
 @dp.message_handler(commands=["start"])
-async def start(message: types.Message):
+async def start(message: Message):
     await message.reply("Hi, I'm a bot, to start guess number game write '/guess")
 
 
 @dp.message_handler(commands=['guess'])
-async def guess(message: types.Message):
+async def guess(message: Message):
     scores = load_scores()
     user_id = str(message.from_user.id)
     if user_id not in scores:
         scores[user_id] = {"score": 0, "attempts": 3}
         save_scores(scores)
-        await message.reply("Welcome to the number guessing game! You have a score of 0 and you have 3 attempts to guess number.")
+    await message.answer("Welcome to the number guessing game! You have a score of 0 and you have 3 attempts to guess number.")
         
     number = random.randint(1, 10)
-    await message.reply("Guess a number between 1 and 10.")
+    await message.answer("Guess a number between 1 and 10.")
 
     await dp.current_state().set_data({'number': number})
 
 
 @dp.message_handler()
-async def guess_number(message: types.Message):
+async def guess_number(message: Message):
     scores = load_scores()
     user_id = str(message.from_user.id)
     state = dp.current_state(user=user_id)
@@ -114,4 +105,4 @@ async def guess_number(message: types.Message):
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
     dbname = get_database()
-    result = dbname["t-bot-result"]
+    result = dbname["guess_number_result"]
